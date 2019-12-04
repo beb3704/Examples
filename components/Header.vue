be975e287c16332a5b064dc55e71a1fbe51f1512
Ike Melancon <ijm0117@p2es.com>
2019-12-04 15:54:25 -0600

    css test

---
+++ components/Header.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <header class="header">
+      <div class="header__logo-box">
+        <img
+          src="~assets/img/logo-white.png"
+          alt="CrownLogo"
+          class="header__logo"
+        />
+      </div>
+      <div class="header__text-box">
+        <h1 class="header__primary">
+          <span class="header__primary-main">Outdoors</span>
+          <span class="header__primary-sub">is where life happens</span>
+        </h1>
+        <a href="#" class="btn btn-white move-in-bottom">Discover our Tours</a>
+      </div>
+    </header>
+
+    <svg width="0" height="0" xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <clipPath id="svgClipPath" clipPathUnits="objectBoundingBox">
+          <polygon points="0, 0,1, 0, 1, .92, 0, 1" />
+        </clipPath>
+        <use clip-path="url(#svgClipPath)" />
+      </defs>
+    </svg>
+  </div>
+</template>
+<style scoped lang="scss">
+@import '~assets/css/variables.scss';
+
+@keyframes moveInLeft {
+  0% {
+    opacity: 0;
+    transform: translateX(-10rem);
+  }
+  80% {
+    transform: translate(1rem);
+  }
+  100% {
+    opacity: 1;
+    transform: translate(0);
+  }
+}
+
+@keyframes moveInRight {
+  0% {
+    opacity: 0;
+    transform: translateX(10rem);
+  }
+  70% {
+    transform: translate(-0.1rem);
+  }
+  100% {
+    opacity: 1;
+    transform: translate(0);
+  }
+}
+
+.header__text-box {
+  position: absolute;
+  top: 43%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+.header__primary {
+  text-transform: uppercase;
+  color: $text-primary;
+  backface-visibility: hidden;
+  margin-bottom: 6rem;
+}
+.header__primary-main {
+  display: block;
+  font-weight: 400;
+  font-size: 6rem;
+  letter-spacing: 3.5rem;
+
+  animation-name: moveInLeft;
+  animation-duration: 2s;
+  animation-timing-function: ease-out;
+}
+.header__primary-sub {
+  display: block;
+  font-size: 2rem;
+  font-weight: 700;
+  letter-spacing: 1.7rem;
+  margin-left: -1.2rem;
+  animation: moveInRight 2s ease-out;
+}
+.header {
+  height: 95vh;
+  background-image: linear-gradient(to right bottom, #7ed56fc5, #28b485c2),
+    url('~assets/img/hero.jpg');
+  background-size: cover;
+  background-position: top;
+  clip-path: url(#svgClipPath);
+  position: relative;
+}
+
+.header__logo-box {
+  position: absolute;
+  top: 4rem;
+  left: 4rem;
+}
+
+.header__logo {
+  height: 3.5rem;
+}
+</style>
